7a37ba158e01961f361cfae44d0c6e5a152debcb
Joan Teixidó <joan.teixido@bismart.com>
2023-01-31 00:01:50 +0100

    fix port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const io = require('socket.io')(httpServer, {
     cors: { origin: '*' }
 });
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8080
 
 
 const { setMaxIdleHTTPParsers } = require('http');
